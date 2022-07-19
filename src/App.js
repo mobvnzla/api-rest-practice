bdd9939cdbd63e92978883f208df296afd2e47eb
gilberto <gilbertogalindez96@gmail.com>
2022-07-19 00:15:56 -0400

    ya funciona randomSection y con buenas practicas gracias a kevin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React from "react";
-import {HeadingSection} from "./components/jsComponents/heading_section";
-import {RandomSection} from './components/jsComponents/random_section';
-import {UploadSection} from './components/jsComponents/uploading_section';
-import {FavoriteSection} from './components/jsComponents/favorite_section';
+import React, { useCallback, useEffect } from 'react';
+import { HeadingSection } from './components/jsComponents/heading_section';
+import { RandomSection } from './components/jsComponents/random_section';
+import { UploadSection } from './components/jsComponents/uploading_section';
+import { FavoriteSection } from './components/jsComponents/favorite_section';
 import './index.css';
 
 function App() {
+  const urlApi = 'https://api.thecatapi.com/v1/images/search';
+  const [randomImgUrl, setRandomImgUrl] = React.useState('');
+
+  /* async function loadRandomcat() {
+    const respuestaDeLaApi = await fetch(urlApi);
+    const jsonDeLaRespuesta = await respuestaDeLaApi.json();
+    const urlDeLaImagen = jsonDeLaRespuesta[0].url;
+    
+    console.log(urlDeLaImagen)
+    
+    setRandomImgUrl(urlDeLaImagen)
+  } */
+
+  async function comoQuiera() {
+    const respuestaDeLaApi = await fetch(urlApi);
+    const jsonDeLaRespuesta = await respuestaDeLaApi.json();
+    const urlDeLaImagen = jsonDeLaRespuesta[0].url;
+
+    console.log(urlDeLaImagen);
+
+    setRandomImgUrl(urlDeLaImagen);
+  }
+
+  useEffect(() => {
+    (() => {
+      comoQuiera();
+    })();
+
+    return () => {};
+  }, []);
+
+  const loadRandomcat = useCallback(() => {
+    comoQuiera();
+  }, []);
+
   return (
     <>
-    <HeadingSection />
+      <HeadingSection />
 
-    <RandomSection />
+      <RandomSection url={randomImgUrl} loadRandomcat={loadRandomcat} />
 
-    <UploadSection /> 
+      <UploadSection />
 
-    <FavoriteSection />
+      <FavoriteSection />
     </>
   );
 }
--- src/components/jsComponents/random_section.js
+++ src/components/jsComponents/random_section.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../cssComponents/randomSection.css';
 
-function RandomSection() {
-    
+function RandomSection({url, loadRandomcat}) {
+   /*  console.log(url) */
     return(
         <div className="random-section">
             <h2
@@ -10,9 +10,13 @@ function RandomSection() {
             >Kittens of the World</h2>
 
             <div className="random-card">
-                <img className="random-card__img" />
+                <img className="random-card__img"
+                src={url}
+                alt='random img' />
                 <span className="random-card__fav-btn"></span>
             </div>
+            <button className='reload'
+            onClick={() => loadRandomcat()}>Reload</button>
         </div>
     );
 }
